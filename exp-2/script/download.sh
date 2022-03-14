e93f3a048d0185a5d586a0f4a15a4221d42594ae
Dennis Angemi <dennisangemi@gmail.com>
2022-03-14 14:14:46 +0100

    update script

--- exp-2/script/download.sh
+++ exp-2/script/download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# this script download csv file from google sheet
+
 curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-wlwiuh5DDUhU_e-6ArOxfwhZGByX_0iNNh5woRCv14tC7gKqGMi9AZnR4ffdUW0Suvegsn3rT1fF/pub?gid=0&single=true&output=csv" > ../data/experimental-data-1.csv
 curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-wlwiuh5DDUhU_e-6ArOxfwhZGByX_0iNNh5woRCv14tC7gKqGMi9AZnR4ffdUW0Suvegsn3rT1fF/pub?gid=1208652346&single=true&output=csv" > ../data/experimental-data-2.csv
 curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vR-wlwiuh5DDUhU_e-6ArOxfwhZGByX_0iNNh5woRCv14tC7gKqGMi9AZnR4ffdUW0Suvegsn3rT1fF/pub?gid=801117007&single=true&output=csv" > ../data/tools.csv
--- exp-3/script/download.sh
+++ exp-3/script/download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# this script download csv file from google sheet
+
 curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vRsnbwUdgR1mJ93T8UzvJ2FddOUyKQQg16RxrLidZLF7ZKuUecHrw1sSFC8jCmoJb2omr2jBJ7w04Jf/pub?gid=0&single=true&output=csv" > ../data/experimental-data-1.csv
 curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vRsnbwUdgR1mJ93T8UzvJ2FddOUyKQQg16RxrLidZLF7ZKuUecHrw1sSFC8jCmoJb2omr2jBJ7w04Jf/pub?gid=902704345&single=true&output=csv" > ../data/experimental-data-2.csv
 curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vRsnbwUdgR1mJ93T8UzvJ2FddOUyKQQg16RxrLidZLF7ZKuUecHrw1sSFC8jCmoJb2omr2jBJ7w04Jf/pub?gid=174989412&single=true&output=csv" > ../data/tools.csv
