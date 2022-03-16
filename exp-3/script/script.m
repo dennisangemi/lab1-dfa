2dcf00baea1cdb56cf1fffdf1bbe3c5f610e369e
Dennis Angemi <dennisangemi@gmail.com>
2022-03-16 17:02:16 +0100

    added output data and img

--- exp-3/script/script.m
+++ exp-3/script/script.m
@@ -17,7 +17,7 @@ t4=df2.t1(41:50,:);
 s1=df1.value(1);
 s2=df1.value(2);
 ds=df1.uncertainty(1);
-dm=df1.uncertainty(5);
+dm=df1.uncertainty(5)./1000;
 dt=df2.uncertainty(2);
 
 % calculating
@@ -39,7 +39,8 @@ s=s1-s2;
 
 % acceleration
 a=zeros(lt,1);
-g=9.81;
+g=9.8;
+dg=0.1;
 
 for i=1:lt
     a(i)=(2.*s)./(tm(i))^2;
@@ -59,27 +60,54 @@ m=m/1000
 
 % force
 F=zeros(lt,1);
+dF=zeros(lt,1);
+ref=zeros(lt,1)
 for i=1:lt
     F(i)=m(i).*(g-a(i));
+    dF(i)=((g-a(i))*2*dm)+(m(i)*dg)+(m(i)*((2.*ds+((4.*s.*dt)/(tm(i))))./(tm(i).^2)));
+    ref(i)=(dF(i)./F(i))*100;
 end
+
+force=horzcat(F,dF,ref)
+
+% propagation of error
+da=zeros(lt,1);
+cfra=zeros(lt,1);
+rea=zeros(lt,1);
+for i=1:lt
+    da(i)=(2.*ds+((4.*s.*dt)/(tm(i))))./(tm(i).^2);
+    cfra(i)=-floor(log10(da(i))); % da sistemare
+    da(i)=round(da(i),cfra(i)); % da sistemare
+    rea(i)=(da(i)./a(i))*100; % da sistemare
+    rea(i)=round(rea(i),2); % da sistemare
+end
+
+acceleration=horzcat(a,da,rea)
 
 % plotting
 scatter=figure
 plot(a,F,'o')
+errorbar(a,F,dF,dF,da,da,'.')
 
-% propagation of error
-da=zeros(lt,1);
-for i=1:lt
-    da(i)=(2.*ds+((4.*s.*dt)/(tm(i))))./(tm(i).^2);
-end
-da
+% testing
+a2=zeros(lt-1,1);
+f2=zeros(lt-1,1);
+a2(1)=a(1);
+a2(2)=a(3);
+a2(3)=a(4);
+a2(4)=a(5);
+
+f2(1)=F(1);
+f2(2)=F(3);
+f2(3)=F(4);
+f2(4)=F(5);
 %%
 % exporting csv
-%writetable(array2table(name-array,'VariableNames',{'acceleration','uncertainty','relative_error'}),'..\data\output-data-1.csv','Delimiter',',','Encoding','UTF-8')
-%writetable(array2table(name-array,'VariableNames',{'coefficient_friction','uncertainty','relative_error'}),'..\data\output-data-2.csv','Delimiter',',','Encoding','UTF-8')
+writetable(array2table(acceleration,'VariableNames',{'acceleration','uncertainty','relative_error'}),'..\data\output-data-1.csv','Delimiter',',','Encoding','UTF-8')
+writetable(array2table(force,'VariableNames',{'force','uncertainty','relative_error'}),'..\data\output-data-2.csv','Delimiter',',','Encoding','UTF-8')
 
 % exporting img
-%saveas(name-figure,'..\img\img-1.png');
+saveas(scatter,'..\img\img-1.png');
 %saveas(name-figure,'..\img\img-2.png');
 
 % exporting mlx2m
