930370483b6b47df5d17fe23ab879b78177939f6
Dennis Angemi <dennisangemi@gmail.com>
2022-03-17 18:45:13 +0100

    added experimental-data-3.csv (blank)

--- exp-3/script/download.sh
+++ exp-3/script/download.sh
@@ -4,4 +4,5 @@
 
 curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vRsnbwUdgR1mJ93T8UzvJ2FddOUyKQQg16RxrLidZLF7ZKuUecHrw1sSFC8jCmoJb2omr2jBJ7w04Jf/pub?gid=0&single=true&output=csv" > ../data/experimental-data-1.csv
 curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vRsnbwUdgR1mJ93T8UzvJ2FddOUyKQQg16RxrLidZLF7ZKuUecHrw1sSFC8jCmoJb2omr2jBJ7w04Jf/pub?gid=902704345&single=true&output=csv" > ../data/experimental-data-2.csv
+curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vRsnbwUdgR1mJ93T8UzvJ2FddOUyKQQg16RxrLidZLF7ZKuUecHrw1sSFC8jCmoJb2omr2jBJ7w04Jf/pub?gid=1850635192&single=true&output=csv" > ../data/experimental-data-3.csv
 curl -kL "https://docs.google.com/spreadsheets/d/e/2PACX-1vRsnbwUdgR1mJ93T8UzvJ2FddOUyKQQg16RxrLidZLF7ZKuUecHrw1sSFC8jCmoJb2omr2jBJ7w04Jf/pub?gid=174989412&single=true&output=csv" > ../data/tools.csv
