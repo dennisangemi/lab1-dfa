dfa34d2d1b143aa9e667c6bfaa4ee7288b02735a
Dennis Angemi <dennisangemi@gmail.com>
2022-03-25 20:31:47 +0100

    added output closes #36

--- exp-4/script/script.m
+++ exp-4/script/script.m
@@ -12,19 +12,20 @@ hd=unique(df2.distance);
 ld=length(hd);
 
 % defining variable
-l0=1;
-g=9.8;
-dg=0.1;
+l0=1;       % pendulum length
+g=9.8;      % gravitational acceleration
+dg=0.1;     % error gravitational acceleration
+dt=tools.uncertainty(1);    % error t
+dr=tools.uncertainty(2);    % errror distance
 
 % creating empty array
 tm=zeros(ld,1);     % tempi medi
-dtm=zeros(ld,1);    % deviazione standard tempi
+dtm=zeros(ld,1);    % error t
 d=zeros(ld,1);      % distance from CM
-dd=zeros(ld,1);     % delta distance
-tt=zeros(ld,1);     % delta t
+dd=zeros(ld,1);     % error distance
 r=[0:0.0001:0.5];   % theoretical distance
 gc=zeros(ld,1);     % gravitational acceleration calculated
-dgc=zeros(ld,1);    % delta gc
+dgc=zeros(ld,1);    % error gc
 regc=zeros(ld,1);   % relative error gravitational acceleration
 
 % multiply *2 and converttng ms2s
@@ -41,27 +42,26 @@ for i=1:ld
     % calculating mean and converting ms2s
     tm(i)=mean(table2array(df2(df2.distance==string(hd(i)),"time")));
     
-    % standard deviation on t
+    % error t
     % dtm(i)=round(std(table2array(df2(df2.distance==string(hd(i)),"time"))),2);
-    dtm(i)=0.05;
+    dtm(i)=dt;
 
-    % distance from cm
+    % distance from CM
     d(i)=table2array(df1(df1.dimension==string(hd(i)),"value"));
     d(i)=d(i)-50;
     d(i)=d(i)/100; %cm2m
     
-    % errore su d
-    % dd(i)=df1.uncertainty(1)/100; 
-    dd(i)=0.005;
+    % error distance from CM
+    dd(i)=dr;
 
     % gravitational acceleration
     gc(i)=(l0.^2.*pi.^2)./(3.*d(i).*tm(i).^2)+(4.*pi.^2.*d(i))./(tm(i).^2);
     dgc(i)=((pi.^2).*2.*l0.*dd(i))./(3.*d(i).*tm(i).^2)  +  (((((l0.^2).*pi.^2)/(3.*d(i).*tm(i).^4))+(4.*pi^2.*d(i)./tm(i).^4)).*8.*tm(i).*dtm(i))  +  abs(-((l0.^2.*pi.^2)./(3.*d(i).^2.*tm(i).^2)) + (4.*pi.^2)./(tm(i).^2)).*dd(i);
-    regc(i)=dgc(i)./gc(i)*100;
+    regc(i)=round(dgc(i)./gc(i)*100,2);
 end
 
-% view array
-cat(2,gc,dgc,regc)
+% view array2table
+output1=array2table(cat(2,string(hd),d,gc,dgc,regc),"VariableNames",{'configuration','distance_CM','gravitational_acceleration','uncertainty','relative_error'})
 round(mean(gc(2:10)),1)
 round(mean(dgc(2:10)),0)
 
@@ -89,7 +89,7 @@ legend('data','theoretical curve')
 % mean(table2array(df2(df2.distance=="d1","time")))
 %%
 % exporting csv
-% writetable(array2table(namearray,'VariableNames',{'acceleration','uncertainty','uom','relative_error'}),'..\data\output-data-1.csv','Delimiter',',','Encoding','UTF-8')
+writetable(output1,'..\data\output-data-1.csv','Delimiter',',','Encoding','UTF-8')
 
 % exporting img
 saveas(plt,'..\img\plot.png');
